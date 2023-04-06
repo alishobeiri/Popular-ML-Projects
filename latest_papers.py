f868d876990d793698dcc39b655d35efb35bebd7
Ali Shobeiri <alishobeiri@ymail.com>
2023-04-05 19:31:39 -0700

    Fetching the hottest ML papers from Reddit

---
+++ latest_papers.py
@@ -0,0 +1,64 @@
+import os
+import re
+import datetime
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SCORE_FILTER = 25
+
+# note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
+auth = requests.auth.HTTPBasicAuth(os.environ.get("CLIENTID"), os.environ.get("SECRET"))
+
+# here we pass our login method (password), username, and password
+data = {'grant_type': 'password',
+        'username': os.environ.get("USERNAME"),
+        'password': os.environ.get("PASSWORD")}
+
+# setup our header info, which gives reddit a brief description of our app
+headers = {'User-Agent': 'MyBot/0.0.1'}
+
+# send our request for an OAuth token
+res = requests.post('https://www.reddit.com/api/v1/access_token',
+                    auth=auth, data=data, headers=headers)
+
+# convert response to JSON and pull access_token value
+TOKEN = res.json()['access_token']
+
+# add authorization to our headers dictionary
+headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+
+# while the token is valid (~2 hours) we just add headers=headers to our requests
+requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
+
+
+res = requests.get("https://oauth.reddit.com/r/machinelearning/hot?f=flair_name%3A%22Research%22",
+                   headers=headers)
+
+out = []
+# loop through each post retrieved from GET request
+for post in res.json()['data']['children']:
+    _post = post['data']
+
+    # search for the pattern in the string and extract the URL
+    urls = re.findall(r'\((https?://arxiv\.org/\S+)\)', _post['selftext'])
+    for url in urls:
+        # append relevant data to dataframe
+        if _post['score'] > SCORE_FILTER:
+            out.append({
+                    'title': _post['title'],
+                    'paper_url': url,
+                    'score': _post['score'],
+                    'date': datetime.datetime.utcfromtimestamp(_post['created_utc'])
+            })
+
+df = pd.DataFrame(out)
+
+# Save markdown string to file
+with open('README.md', 'w') as f:
+    df.to_markdown(f, index=False)
+
+df.to_csv('hottest_ml_papers.csv', index=False)
+
